69013c81510e4cf5303375e8d87e594fa11d6e04
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-06 08:51:51 -0700

    Create random_numbers.py

---
+++ random_numbers.py
@@ -0,0 +1,21 @@
+# Your Name: Shubham
+import random
+
+def howManyUntil(stopNum):
+    times = 1
+    i = random.randrange(0,99)
+    while i != stopNum:
+        times += 1
+        #print(i)
+        i = random.randrange(0,99)
+    print('---------------------found-----------------------------')
+    return times
+
+# define howManyUntil function
+
+# call it to test it out
+# to test it you may want to make a smaller version that generates random numbers from 0 to 10
+# and prints out the random numbers....
+
+howManyUntil(22)
+
