42360cc6209083be956ab587c141d5b8e548e630
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-11-06 08:54:40 -0700

    Create approximate_e.py

---
+++ approximate_e.py
@@ -0,0 +1,18 @@
+# My Name: Shubham
+
+# don't forget to import the math library... ok lol
+import math
+
+def approxE(precision):
+    calculatede = 0
+    i = 0
+ 
+    while abs(math.e-calculatede) > precision:
+        calculatede += (1/math.factorial(i))
+        i += 1
+   
+    print('The value of e with precision',precision,'was calculated using',i,'iterations.')
+    
+    return(calculatede)
+
+print(approxE(0.03))
